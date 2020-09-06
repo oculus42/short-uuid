60052da6d5b0be8602948b0c68798ebd23f722ae
Samuel Rouse <samuel.rouse@gmail.com>
2020-09-05 23:08:55 -0400

    es6

--- index.js
+++ index.js
@@ -3,33 +3,34 @@
  * Simple wrapper functions to produce shorter UUIDs for cookies, maybe everything?
  */
 
-var anyBase = require('any-base');
-var uuidV4 = require('uuid').v4;
+const anyBase = require('any-base');
+const uuidV4 = require('uuid').v4;
 
-var flickrBase58 = '123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ';
-var cookieBase90 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+-./:<=>?@[]^_`{|}~";
+const flickrBase58 = '123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ';
+const cookieBase90 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+-./:<=>?@[]^_`{|}~";
 
-var baseOptions = {
-    consistentLength: false,
+const baseOptions = {
+  consistentLength: true,
 };
 
-var toFlickr;
+let toFlickr;
 
 /**
  * Takes a UUID, strips the dashes, and translates.
  * @param {string} longId
  * @param {function(string)} translator
+ * @param {Object} [paddingParams]
  * @returns {string}
  */
-function shortenUUID (longId, translator, paddingParams) {
-    var translated = translator(longId.toLowerCase().replace(/-/g, ""));
+function shortenUUID(longId, translator, paddingParams) {
+  var translated = translator(longId.toLowerCase().replace(/-/g, ""));
 
-    if (!paddingParams || !paddingParams.consistentLength) return translated;
+  if (!paddingParams || !paddingParams.consistentLength) return translated;
 
-    return translated.padStart(
-        paddingParams.shortIdLength,
-        paddingParams.paddingChar
-    );
+  return translated.padStart(
+    paddingParams.shortIdLength,
+    paddingParams.paddingChar
+  );
 }
 
 /**
@@ -39,94 +40,98 @@ function shortenUUID (longId, translator, paddingParams) {
  * @returns {string}
  */
 function enlargeUUID(shortId, translator) {
-    var uu1 = translator(shortId);
-    var leftPad = "";
-    var m;
+  var uu1 = translator(shortId);
+  var leftPad = "";
+  var m;
 
-    // Pad out UUIDs beginning with zeros (any number shorter than 32 characters of hex)
-    for (var i = 0, len = 32-uu1.length; i < len; ++i) {
-        leftPad += "0";
-    }
+  // Pad out UUIDs beginning with zeros (any number shorter than 32 characters of hex)
+  for (var i = 0, len = 32 - uu1.length; i < len; ++i) {
+    leftPad += "0";
+  }
 
-    // Join the zero padding and the UUID and then slice it up with match
-    m = (leftPad + uu1).match(/(\w{8})(\w{4})(\w{4})(\w{4})(\w{12})/);
+  // Join the zero padding and the UUID and then slice it up with match
+  m = (leftPad + uu1).match(/(\w{8})(\w{4})(\w{4})(\w{4})(\w{12})/);
 
-    // Accumulate the matches and join them.
-    return [m[1], m[2], m[3], m[4], m[5]].join('-');
+  // Accumulate the matches and join them.
+  return [m[1], m[2], m[3], m[4], m[5]].join('-');
 }
 
 // Calculate length for the shortened ID
 function getShortIdLength(alphabetLength) {
-    return Math.ceil(Math.log(2**128) / Math.log(alphabetLength));
+  return Math.ceil(Math.log(2 ** 128) / Math.log(alphabetLength));
 }
 
-module.exports = (function(){
+module.exports = (function () {
 
-    /**
-     * @constructor
-     * @param {string?, object?} toAlphabet - Defaults to flickrBase58 if not provided
-     * @returns {{new: (function()),
-     *  uuid: (function()),
-     *  fromUUID: (function(string)),
-     *  toUUID: (function(string)),
-     *  alphabet: (string)}}
-     */
-    function MakeConvertor(toAlphabet, options) {
+  /**
+   * @constructor
+   * @param {string} toAlphabet - Defaults to flickrBase58 if not provided
+   * @param {Object} [options]
+   *
+   * @returns {{new: (function()),
+   *  uuid: (function()),
+   *  fromUUID: (function(string)),
+   *  toUUID: (function(string)),
+   *  alphabet: (string)}}
+   */
+  function MakeConvertor(toAlphabet, options) {
 
-        // Default to Flickr 58
-        var useAlphabet = toAlphabet || flickrBase58;
+    // Default to Flickr 58
+    const useAlphabet = toAlphabet || flickrBase58;
 
-        // Default to baseOptions
-        var selectedOptions = {...baseOptions, ...options};
+    // Default to baseOptions
+    const selectedOptions = {...baseOptions, ...options};
 
-        // Check alphabet for duplicate entries
-        if ([...new Set(Array.from(useAlphabet))].length !== useAlphabet.length) {
-            throw new Error('The provided Alphabet has duplicate characters resulting in unreliable results');
-        }
-                  
-        // Padding Params
-        var paddingParams = {
-            consistentLength: selectedOptions.consistentLength,
-            shortIdLength: getShortIdLength(useAlphabet.length),
-            paddingChar: useAlphabet[0],
-        };
-
-        // UUIDs are in hex, so we translate to and from.
-        var fromHex = anyBase(anyBase.HEX, useAlphabet);
-        var toHex = anyBase(useAlphabet, anyBase.HEX);
-        var generate = function() {
-            return shortenUUID(uuidV4(), fromHex, paddingParams);
-        };
-
-        return {
-            new: generate,
-            generate: generate,
-            uuid: uuidV4,
-            fromUUID: function(uuid) {
-                return shortenUUID(uuid, fromHex, paddingParams);
-            },
-            toUUID: function(shortUuid) { return enlargeUUID(shortUuid, toHex); },
-            alphabet: useAlphabet
-        };
+    // Check alphabet for duplicate entries
+    if ([...new Set(Array.from(useAlphabet))].length !== useAlphabet.length) {
+      throw new Error('The provided Alphabet has duplicate characters resulting in unreliable results');
     }
 
-    // Expose the constants for other purposes.
-    MakeConvertor.constants = {
-        flickrBase58: flickrBase58,
-        cookieBase90: cookieBase90
+    // Padding Params
+    const paddingParams = {
+      consistentLength: selectedOptions.consistentLength,
+      shortIdLength: getShortIdLength(useAlphabet.length),
+      paddingChar: useAlphabet[0],
     };
 
-    // Expose the generic v4 UUID generator for convenience
-    MakeConvertor.uuid = uuidV4;
-
-    // Provide a generic generator
-    MakeConvertor.generate = function() {
-        if (!toFlickr) {
-            // Generate on first use;
-            toFlickr = anyBase(anyBase.HEX, flickrBase58);
-        }
-        return shortenUUID(uuidV4(), toFlickr);
+    // UUIDs are in hex, so we translate to and from.
+    const fromHex = anyBase(anyBase.HEX, useAlphabet);
+    const toHex = anyBase(useAlphabet, anyBase.HEX);
+    const generate = function () {
+      return shortenUUID(uuidV4(), fromHex, paddingParams);
     };
 
-    return MakeConvertor;
+    return {
+      new: generate,
+      generate: generate,
+      uuid: uuidV4,
+      fromUUID: function (uuid) {
+        return shortenUUID(uuid, fromHex, paddingParams);
+      },
+      toUUID: function (shortUuid) {
+        return enlargeUUID(shortUuid, toHex);
+      },
+      alphabet: useAlphabet
+    };
+  }
+
+  // Expose the constants for other purposes.
+  MakeConvertor.constants = {
+    flickrBase58: flickrBase58,
+    cookieBase90: cookieBase90
+  };
+
+  // Expose the generic v4 UUID generator for convenience
+  MakeConvertor.uuid = uuidV4;
+
+  // Provide a generic generator
+  MakeConvertor.generate = function () {
+    if (!toFlickr) {
+      // Generate on first use;
+      toFlickr = anyBase(anyBase.HEX, flickrBase58);
+    }
+    return shortenUUID(uuidV4(), toFlickr);
+  };
+
+  return MakeConvertor;
 }());
