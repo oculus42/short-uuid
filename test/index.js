028e31186e899bc93e3ca37e48b7b05187bc7345
Samuel Rouse <samuel.rouse@gmail.com>
2024-05-01 08:40:27 -0400

    test: Ensure 100% coverage

--- test/index.js
+++ test/index.js
@@ -290,39 +290,50 @@ test('Default generate quantity tests', (t) => {
 });
 
 test('Validate', (t) => {
-  t.plan(21);
+  t.plan(25);
+
+  // Bad type
+  t.notOk(b36.validate(100), 'only strings');
 
   // Too short
-  t.equal(b36.validate('123'), false, 'uuid25 too short');
-  t.equal(b58.validate('123'), false, 'flickr too short');
-  t.equal(b90.validate('123'), false, 'cookie too short');
+  t.notOk(b36.validate('123'), 'uuid25 too short');
+  t.notOk(b58.validate('123'), 'flickr too short');
+  t.notOk(b90.validate('123'), 'cookie too short');
+
+  // Short is valid without consistentLength
+  const s36 = short(short.constants.uuid25Base36, { consistentLength: false });
+  const s58 = short(short.constants.flickrBase58, { consistentLength: false });
+  const s90 = short(short.constants.cookieBase90, { consistentLength: false });
+  t.ok(s36.validate('111'));
+  t.ok(s58.validate('111'));
+  t.ok(s90.validate('111'));
 
   // Too long
-  t.equal(b36.validate('123456789012345678901234567890'), false, 'uuid25 too long');
-  t.equal(b58.validate('123456789012345678901234567890'), false, 'flickr too long');
-  t.equal(b90.validate('123456789012345678901234567890'), false, 'cookie too long');
+  t.notOk(b36.validate('123456789012345678901234567890'), 'uuid25 too long');
+  t.notOk(b58.validate('123456789012345678901234567890'), 'flickr too long');
+  t.notOk(b90.validate('123456789012345678901234567890'), 'cookie too long');
 
   // Bad alphabet
-  t.equal(b36.validate('123456789012345678901234"'), false, 'uuid25 validates alphabet');
-  t.equal(b58.validate('123456789012345678901"'), false, 'flickr validates alphabet');
-  t.equal(b90.validate('1234567890123456789"'), false, 'cookie validates alphabet');
+  t.notOk(b36.validate('123456789012345678901234"'), 'uuid25 validates alphabet');
+  t.notOk(b58.validate('123456789012345678901"'), 'flickr validates alphabet');
+  t.notOk(b90.validate('1234567890123456789"'), 'cookie validates alphabet');
 
   // Generated value passes
-  t.equal(b36.validate(b36.generate()), true, 'uuid25 validates');
-  t.equal(b58.validate(b58.generate()), true, 'flickr validates');
-  t.equal(b90.validate(b90.generate()), true, 'cookie validates');
+  t.ok(b36.validate(b36.generate()), 'uuid25 validates');
+  t.ok(b58.validate(b58.generate()), 'flickr validates');
+  t.ok(b90.validate(b90.generate()), 'cookie validates');
 
   // "empty" value passes without uuid check
-  t.equal(b36.validate('0'), false, 'uuid25 passes bad uuid without check');
-  t.equal(b58.validate('0'), false, 'flickr fails bad uuid without check');
-  t.equal(b90.validate('0'), false, 'cookie fails bad uuid without check');
+  t.notOk(b36.validate('0'), 'uuid25 passes bad uuid without check');
+  t.notOk(b58.validate('0'), 'flickr fails bad uuid without check');
+  t.notOk(b90.validate('0'), 'cookie fails bad uuid without check');
 
   // With uuid check
-  t.equal(b36.validate(b36.generate(), true), true, 'uuid25 validates uuid');
-  t.equal(b58.validate(b58.generate(), true), true, 'flickr validates uuid');
-  t.equal(b90.validate(b90.generate(), true), true, 'cookie validates uuid');
+  t.ok(b36.validate(b36.generate(), true), 'uuid25 validates uuid');
+  t.ok(b58.validate(b58.generate(), true), 'flickr validates uuid');
+  t.ok(b90.validate(b90.generate(), true), 'cookie validates uuid');
 
-  t.equal(b36.validate('0', true), false, 'uuid25 fails bad uuid');
-  t.equal(b58.validate('0', true), false, 'flickr fails bad uuid');
-  t.equal(b90.validate('0', true), false, 'cookie fails bad uuid');
+  t.notOk(b36.validate('0', true), 'uuid25 fails bad uuid');
+  t.notOk(b58.validate('0', true), 'flickr fails bad uuid');
+  t.notOk(b90.validate('0', true), 'cookie fails bad uuid');
 });
