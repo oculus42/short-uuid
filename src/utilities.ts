cca45a2903724051d827eb690762325849b684dd
oculus42 <oculus.habent@gmail.com>
2025-03-09 12:06:12 -0400

    Split out utilities

---
+++ src/utilities.ts
@@ -0,0 +1,12 @@
+/**
+ * Calculate length for the shortened ID
+ * @param {number} alphabetLength
+ * @returns {number}
+ */
+export const calculateMaxLength = (alphabetLength:number):number => (
+  Math.ceil(Math.log(2 ** 128) / Math.log(alphabetLength)));
+
+export const checkForDuplicates = (alphabet:string):boolean => {
+  const alphabetSet = new Set(alphabet);
+  return alphabet.length !== alphabetSet.size;
+};
