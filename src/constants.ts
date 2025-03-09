cc2d99ca5a8a8cb9b45a49e2657af63b93c58d3e
oculus42 <oculus.habent@gmail.com>
2025-03-09 12:05:18 -0400

    Split constants to a file

---
+++ src/constants.ts
@@ -0,0 +1,18 @@
+// cookieBase90 is my own creation
+export const cookieBase90 = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+-./:<=>?@[]^_`{|}~";
+
+// flickrBase58 from https://www.flickr.com/groups/api/discuss/72157616713786392/
+export const flickrBase58 = '123456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ';
+
+// uuid25Base36 from https://www.npmjs.com/package/uuid25
+export const uuid25Base36 = '0123456789abcdefghijklmnopqrstuvwxyz';
+
+// case32 from https://datatracker.ietf.org/doc/html/rfc4648#section-6
+export const rfcBase32 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567';
+
+export default {
+  cookieBase90,
+  flickrBase58,
+  uuid25Base36,
+  rfcBase32,
+};
