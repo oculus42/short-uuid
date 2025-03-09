741956018f5bbf41acd596462813f12e1533b036
oculus42 <oculus.habent@gmail.com>
2025-03-09 12:08:33 -0400

    Move the core convertor logic into a file

---
+++ src/convertor.ts
@@ -0,0 +1,55 @@
+import anyBase from "any-base";
+
+import type {config, UUID, SUUID, translator} from "./types";
+
+import defaultConfig from "./config";
+import {calculateMaxLength, checkForDuplicates } from "./utilities";
+import validate from "./validate";
+import { shortenUUID, restoreUUID } from "./translate";
+
+const createTranslatorFromOptions = (options:object = {}):translator => {
+  const setup = {
+    ...defaultConfig,
+    ...options,
+  };
+
+  // Check for duplicate characters in the alphabet.
+  if (checkForDuplicates(setup.alphabet)) {
+    throw new Error('Alphabet contains duplicate characters.');
+  }
+
+  const config = {
+    ...setup,
+    maxLength: calculateMaxLength(setup.alphabet.length),
+    // TODO Allow a padding character from options?
+    paddingCharacter: setup.alphabet[0],
+    hexFromAlphabet: anyBase(setup.alphabet, anyBase.HEX),
+    hexToAlphabet: anyBase(anyBase.HEX, setup.alphabet),
+  } as config;
+
+  const translator = {
+    alphabet: config.alphabet,
+    fromUUID: (uuid:UUID):SUUID => shortenUUID(config, uuid),
+    generate: ():SUUID => shortenUUID(config, config.uuid()),
+    maxLength: config.maxLength,
+    toUUID: (shortUuid:SUUID):UUID => restoreUUID(config, shortUuid),
+    uuid: config.uuid,
+    validate: validate(config),
+  } as translator;
+
+    Object.freeze(translator);
+    return translator;
+}
+
+const createTranslatorFromAlphabet = (alphabet:string, options:object = {}):translator => {
+  return createTranslatorFromOptions({
+    ...options,
+    alphabet,
+  });
+};
+
+export const createTranslator = (arg?:string|object, options:object = {}) : translator => {
+  return typeof arg === 'string'
+    ? createTranslatorFromAlphabet(arg, options)
+    : createTranslatorFromOptions(arg);
+}
